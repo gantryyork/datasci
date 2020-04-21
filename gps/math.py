0707454ca25726e656921fb9c4b162a040d2a16c
Gantry York <gantry.york@gmail.com>
2020-04-21 05:33:24 -0700

    Initial commit of gps.math

---
+++ gps/math.py
@@ -0,0 +1,25 @@
+import gps.convert
+
+
+Rearth = 6371 # km
+
+
+# All math is done in decimal degress and km
+
+
+def lon_dist(km):
+    return
+
+def lon_dist_km(ddeg):
+    return
+
+def lat_dist(km):
+    return math.degrees(km / Rearth)
+
+def lat_dist_km(ddeg, lon):
+    return
+
+
+def add_dist(xy, dist):
+
+    return
